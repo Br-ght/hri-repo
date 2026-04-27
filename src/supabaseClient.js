0574fe37f844d4de3e3d5963df857a00d51d4454
Bright Nathaniel Agyapong <83359955+Br-ght@users.noreply.github.com>
2026-04-27 13:58:15 -0400

    client keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = 'https://https://eoitszrznqmmqbhfnlln.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvaXRzenJ6bnFtbXFiaGZubGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwMjY1NTYsImV4cCI6MjA4NTYwMjU1Nn0.qvvP9bNbv1b07O9yLCmUZDrtDe-z-033z6N42VvuX2Y'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
